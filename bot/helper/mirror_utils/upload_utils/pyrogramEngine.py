ca360df70a565b9d704cda7b164df186d667c7e7
PachGit <108862443+PachGit@users.noreply.github.com>
2022-09-05 10:43:27 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -69,13 +69,13 @@ class TgUploader:
 
     def __upload_file(self, up_path, file_, dirpath):
         if CUSTOM_FILENAME is not None:
-            cap_mono = f"{CUSTOM_FILENAME} <b><i>{file_}</b></i><b>\n\n<a href='https://t.me/T4TVSERIES1'>@T4TVSeries</a>    | @P4PSa\nT4TVSeries ™  |  P4PSa  ™ </b>"
+            cap_mono = f"{CUSTOM_FILENAME} <b><i>{file_}</b></i><b>\n\n<a href='https://t.me/T4TVSERIES1'>@T4TVSeries</a>    | <a href='https://t.me/P4PSa2'>@P4PSa</a>\nT4TVSeries ™  |  P4PSa  ™ </b>"
             file_ = f"{CUSTOM_FILENAME} {file_}"
             new_path = ospath.join(dirpath, file_)
             osrename(up_path, new_path)
             up_path = new_path
         else:
-            cap_mono = f"<b><i>{file_}</b></i><b>\n\n<a href='https://t.me/T4TVSERIES1'>@T4TVSeries</a>    | @P4PSa\nT4TVSeries ™  |  P4PSa  ™ </b>"
+            cap_mono = f"<b><i>{file_}</b></i><b>\n\n<a href='https://t.me/T4TVSERIES1'>@T4TVSeries</a>    | <a href='https://t.me/P4PSa2'>@P4PSa</a>\nT4TVSeries ™  |  P4PSa  ™ </b>"
         notMedia = False
         thumb = self.__thumb
         self.__is_corrupted = False
